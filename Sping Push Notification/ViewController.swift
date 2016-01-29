d2cd9b8188cb563f82347d83b3d1064d8b3e1a87
Johnny Jiang <Johnny.Jiang@suncorp.com.au>
2016-01-29 11:42:30 +1100

    Initial Commit

---
+++ Sping Push Notification/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sping Push Notification
+//
+//  Created by Johnny Jiang on 29/01/2016.
+//  Copyright © 2016 sping.com.au. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Sping Push NotificationTests/Sping_Push_NotificationTests.swift
@@ -0,0 +1,36 @@
+//
+//  Sping_Push_NotificationTests.swift
+//  Sping Push NotificationTests
+//
+//  Created by Johnny Jiang on 29/01/2016.
+//  Copyright © 2016 sping.com.au. All rights reserved.
+//
+
+import XCTest
+@testable import Sping_Push_Notification
+
+class Sping_Push_NotificationTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
